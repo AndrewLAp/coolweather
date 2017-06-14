caad33afba4668a32df3590730628f09d4755f56
Andrew <464691894@qq.com>
2017-06-14 16:08:39 +0800

    加入显示天气功能和更新天气功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by zuying on 2017/6/12.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
